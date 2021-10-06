119ffde74199c824daeab362371becdbbaf8e66e
BeCrutch <roman.naumenko.94@gmail.com>
2021-10-06 13:32:48 +0300

    upDate script crm

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -30,12 +30,21 @@
 
 <script src="https://code.jquery.com/jquery-3.5.0.js"></script>
 <script src="{{ asset('./js/script.js') }}"></script>
+
+{{-- old: testing --}}
+{{--<script>--}}
+    {{--(function(w,d,u){--}}
+        {{--var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/60000|0);--}}
+        {{--var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);--}}
+    {{--})(window,document,'https://sandi.com.ua/upload/crm/site_button/loader_2_s7bdpb.js');--}}
+{{--</script>--}}
+
+{{-- new: 06.10.21 --}}
 <script>
     (function(w,d,u){
         var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/60000|0);
         var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
-    })(window,document,'https://sandi.com.ua/upload/crm/site_button/loader_2_s7bdpb.js');
+    })(window,document,'https://sandi.com.ua/upload/crm/site_button/loader_4_5x66p1.js');
 </script>
-
 
 @endsection
