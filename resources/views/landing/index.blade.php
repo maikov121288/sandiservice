b73cdfb95ad43058f40e759f0d02f432b86c9fab
maikov121288 <maikov121288@gmail.com>
2021-10-15 10:26:29 +0300

    f

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -40,11 +40,13 @@
 {{--</script>--}}
 
 {{-- new: 06.10.21 --}}
+
 <script>
     (function(w,d,u){
         var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/60000|0);
         var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
     })(window,document,'https://sandi.com.ua/upload/crm/site_button/loader_4_5x66p1.js');
 </script>
+
 
 @endsection
