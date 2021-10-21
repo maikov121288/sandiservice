5daf6f3adb0f4bf1bf83f9f92ca7146cd7af501b
Roman <romanchaban9@gmail.com>
2021-10-21 10:41:03 +0300

    fixed console-bugs

--- resources/js/site/app.js
+++ resources/js/site/app.js
@@ -2,7 +2,6 @@
 require('./components/menu.js');
 require('./components/links.js');
 require('./components/circles.js');
-//require('./components/callBack.js');
 require('./components/toTopBtn.js');
 require('./components/slider.js');
 
--- resources/js/site/components/circles.js
+++ resources/js/site/components/circles.js
@@ -1,6 +1,18 @@
 (function() {
     const circulars = document.querySelectorAll('.moto-widget-completion_bar_circular__svg');
 
+    const come = function (elem) {
+        var docViewTop = $(window).scrollTop(),
+            docViewBottom = docViewTop + $(window).height(),
+            elemTop = window.pageYOffset + elem.getBoundingClientRect().top;
+        //elemBottom = elemTop + $(elem).height();
+
+        if ((elemTop <= docViewBottom) && (elemTop >= docViewTop)) {
+            elem.classList.add('moto-widget-completion_bar_circular__svg-animated');
+        }
+        ;
+    }
+
     window.addEventListener('scroll', function () {
         for (let i = 0; i < circulars.length; i++) {
             come(circulars[i]);
